4c3c294e459afb939ab61af791cb8482e000b07b
Warren Gerald Polandra <85095564+warrenpolandra@users.noreply.github.com>
2022-12-21 11:52:50 +0700

    Update config.php

--- config.php
+++ config.php
@@ -5,11 +5,11 @@ $user = "root";
 $password = "";
 $nama_database = "ujian_kkp";
 
-$db = mysqli_connect($server, $user, $password, $nama_database, "8111");
+$db = mysqli_connect($server, $user, $password, $nama_database);
 if( !$db ){
     die("Gagal terhubung dengan database: " . mysqli_connect_error());
 }
 
-$pdo = new PDO('mysql:host='.$server.';port=8111;dbname='.$nama_database, $user, $password);
+$pdo = new PDO('mysql:host='.$server.';dbname='.$nama_database, $user, $password);
 
 ?>
